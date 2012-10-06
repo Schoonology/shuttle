faa9a3676816eae78cda20130e2c6cb46a9a1433
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2012-10-06 15:50:34 -0700

    Added a Bridge class (what the router used to be).

---
+++ test/customer-bridge-service.js
@@ -0,0 +1,45 @@
+/*global describe:true, it:true, before:true, after:true, beforeEach:true, afterEach:true */
+var expect = require('chai').expect,
+    shuttle = require('..');
+
+describe('Consumer-Bridge-Service Relationship', function () {
+    before(function () {
+        this.service = new shuttle.Service();
+        this.bridge = new shuttle.Bridge();
+        this.consumer = new shuttle.Consumer();
+
+        this.service.on('test', function (data, callback) {
+            expect(data).to.have.property('answer', 42);
+            callback(null, {
+                ok: true
+            });
+        });
+    });
+
+    beforeEach(function () {
+        // Using the IPC transport instead of TCP as TCP causes race conditions when used within the same process.
+        this.serviceUrl = '/tmp/' + Math.random().toString().slice(2);
+        this.consumerUrl = '/tmp/' + Math.random().toString().slice(2);
+    });
+
+    afterEach(function () {
+        this.service.close();
+        this.bridge.close();
+        this.consumer.close();
+    });
+
+    it('should support Bridge listening, Service and Consumer connecting', function (done) {
+        this.bridge.listenForServices(this.serviceUrl);
+        this.bridge.listenForConsumers(this.consumerUrl);
+        this.service.connect(this.serviceUrl);
+        this.consumer.connect(this.consumerUrl);
+
+        this.consumer.send('test', {
+            answer: 42
+        }, function (err, response) {
+            expect(err).to.not.exist;
+            expect(response).to.have.property('ok', true);
+            done();
+        });
+    });
+});
