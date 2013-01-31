4587c24b58bc1677c0074fcab0a33e6b31d15195
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-01-30 17:47:52 -0800

    Added a quick hack to prevent Shuttle from blocking under extreme load.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 module.exports = require('./lib/shuttle');
+
+// HACK - Hitting the HWM can block the process completely. To unblock applications already using Shuttle,
+// I've added this hack to unblock them periodically.
+setInterval(console.log, 100)
